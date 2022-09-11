5d95e02f9ae50345b0695e26e303ceac8fef4a8c
SHUBHAM SAINI <SHUBHAM SAINI@DESKTOP-89I46OA>
2022-09-11 12:15:52 +0530

    This is my first commit

---
+++ TestSpringBootByDurgesh/src/main/java/com/ibm/springboot/TestSpringBootByDurgeshApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootByDurgeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringBootByDurgeshApplication.class, args);
+	}
+
+ }
---
+++ TestSpringBootByDurgesh/src/test/java/com/ibm/springboot/TestSpringBootByDurgeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringBootByDurgeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
